63d12a509a008610e5f6a4e6bd3c2cc3b09b4709
Luiza Ribeiro <84139409+luizaribeiro06@users.noreply.github.com>
2023-05-28 22:05:58 -0300

    Add files via upload

---
+++ src/Data.jsx
@@ -0,0 +1,60 @@
+const DATA = [
+    {
+        id : 0,
+        title : "penis vermelho",
+        price : 750,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "https://i.imgflip.com/4cxvd2.jpg?a467640"
+    },
+    {
+        id : 1,
+        title : "penis vermelho",
+        price : 800,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "https://i.imgflip.com/4cxvd2.jpg?a467640"
+    },
+    {
+        id : 2,
+        title : "penis vermelho",
+        price : 950,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "https://i.imgflip.com/4cxvd2.jpg?a467640"
+    },
+    {
+        id : 3,
+        title : "penis vermelho",
+        price : 1050,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "https://i.imgflip.com/4cxvd2.jpg?a467640"
+    },
+    {
+        id : 4,
+        title :"penis vermelho",
+        price : 1150,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "https://i.imgflip.com/4cxvd2.jpg?a467640"
+    },
+    {
+        id : 5,
+        title : "penis vermelho",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "https://i.imgflip.com/4cxvd2.jpg?a467640"
+    },
+    {
+        id : 6,
+        title : "penis vermelho",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "https://i.imgflip.com/4cxvd2.jpg?a467640"
+    },
+    {
+        id : 7,
+        title : "penis vermelho",
+        price : 1250,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "https://i.imgflip.com/4cxvd2.jpg?a467640"
+    }
+]
+
+export default DATA;
